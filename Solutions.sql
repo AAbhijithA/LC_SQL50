7a3a0c9a9fb00e405e15acf84151328567ca8a40
Abhijith Ajith <113827354+AAbhijithA@users.noreply.github.com>
2023-12-10 02:21:07 +0530

    Update Solutions.sql

--- Solutions.sql
+++ Solutions.sql
@@ -14,3 +14,26 @@ select distinct author_id as id from Views where author_id = viewer_id order by 
 select tweet_id from Tweets where content like '________________%';
 /*OR*/
 select tweet_id from Tweets where CHAR_LENGTH(content) > 15;
+
+/*6*/
+select U.unique_id, E.name from Employees E left join EmployeeUNI U on U.id = E.id;
+
+/*7*/
+select product_name, year, price from Sales, Product where Sales.product_id = Product.product_id;
+
+/*8*/
+select customer_id, count(*) as count_no_trans 
+from Visits 
+where visit_id not in (select visit_id from Transactions) group by customer_id;
+
+/*9*/
+/* datediff() is a function that returns the number of days of difference between 2 dates */
+select w1.id from Weather w1, Weather w2 
+where datediff(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+
+/*10*/
+/* ROUND() rounds it off to nearest decimal so here we needed to nearest 3rd decimal place */
+select a.machine_id, ROUND(AVG(b.timestamp - a.timestamp),3) as processing_time 
+from Activity a, Activity b 
+where a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start' and b.activity_type = 'end' 
+group by a.machine_id; 
